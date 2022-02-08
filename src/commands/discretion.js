792a7d76d8b38e01543b9f72bee5bb3b5526b7ec
Sebastian Wu <68798342+sebswU@users.noreply.github.com>
2022-02-08 18:05:12 -0500

    still not tested but has been organized

---
+++ src/commands/discretion.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    name: "discredtion",
+    run: async(suggestor, suspect, discretionFlag) => {//removes any wrongdoing actions by bot
+            if (msg.author.bot) return//so that the bot does not react to itself
+
+           
+      }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,50 @@
-const discord = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-
+const { Client, Collection, Intents } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { token } = require('./config.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+//discordjs command files
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
-};
-client.on('ready', () => {//all client. functions are arrow functions
-    console.log("active now")
-})
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+//discordjs EventEmitter class
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+//allows us to use external files and data from command files through 'require()'
+//"Collection" => scripts in the file called "commands"
 
 client.commands = new Collection();
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-client.login(process.env['TOKEN'])
+
+
+//runs when started/activated
+client.once('ready', () => {//all client. functions are arrow functions
+    console.log("active now");
+})
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {//bad practice to do if/else and the bot may crash under error
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.login(token);

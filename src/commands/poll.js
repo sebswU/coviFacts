32793de245be5ccb0caf8b2b004320b14e0c3877
Sebastian Wu <68798342+sebswU@users.noreply.github.com>
2022-03-17 14:14:55 -0400

    poll.js in tensorflow branch

--- src/commands/poll.js
+++ src/commands/poll.js
@@ -7,7 +7,7 @@ module.exports = {
     .setDescription('Input a word and the bot will make a poll in the polling channel')
     .addStringOption(option =>
         option.setName('word')
-        .setDescription('Suggest new word to the administrator')
+        .setDescription('Suggest new word to the administrator via a poll message')
         .setRequired(true)
         )
 }

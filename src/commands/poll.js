1afaa096a02a71ef0a3b8d4f67a88e34e4ca0199
Sebastian Wu <68798342+sebswU@users.noreply.github.com>
2022-03-17 14:12:15 -0400

    slash command poll.js registered

---
+++ src/commands/poll.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('poll')
+    .setDefaultPermission(true)
+    .setDescription('Input a word and the bot will make a poll in the polling channel')
+    .addStringOption(option =>
+        option.setName('word')
+        .setDescription('Suggest new word to the administrator')
+        .setRequired(true)
+        )
+}
